e07fd52e78930f3efb0552280d486001738d5d91
David <david.leon.m@gmail.com>
2025-01-24 23:43:36 +0100

    moment and more

--- src/helpers/i18n.ts
+++ src/helpers/i18n.ts
@@ -2,6 +2,7 @@ import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import Backend from 'i18next-http-backend'
 import LanguageDetector from 'i18next-browser-languagedetector'
+import moment from 'moment'
 
 i18n
   .use(initReactI18next)
@@ -23,5 +24,10 @@ i18n
     },
     detection: { order: ['localStorage', 'sessionStorage', 'navigator'] },
   })
+
+i18n.on('languageChanged', (lng) => {
+  moment.locale(lng);
+  console.debug(`Language changed to '${lng}'`);
+})
 
 export default i18n
--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,13 @@ import './index.css'
 import App from './App.tsx'
 import { I18nextProvider } from 'react-i18next'
 import i18n from './helpers/i18n'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-    <I18nextProvider i18n={i18n}></I18nextProvider>
+    <I18nextProvider i18n={i18n}>
+      <QueryClientProvider client={new QueryClient()}></QueryClientProvider>
+      <App />
+    </I18nextProvider>
   </StrictMode>
 )
