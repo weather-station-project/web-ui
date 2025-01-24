ff788855b3d6fb59e7207b14313775fb10df5fe3
David <david.leon.m@gmail.com>
2025-01-24 23:24:58 +0100

    i18n

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,7 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-        </a>
+        <a href="https://vite.dev" target="_blank"></a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
