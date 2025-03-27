22b49e3af5cc2012cf60e0192f5a5122461d1fdc
David <david.leon.m@gmail.com>
2025-03-27 15:53:32 +0100

    e2e tests

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,36 +1,22 @@
 describe('Home', (): void => {
   beforeEach((): void => {
-    cy.intercept(
-      {
-        method: 'GET',
-        url: '/api/auth',
-      },
-      { access_token: '1234567890' }
-    ).as('getAuthToken')
-
     cy.visit('/')
   })
 
   afterEach((): void => {
-    cy.wait('@getAuthToken').its('response.statusCode').should('equal', 200)
+    cy.url().should('match', /\/$/)
   })
 
   it('root elements should be there', (): void => {
     cy.get('h1[data-testid="home.real-time-measurements"]').should('have.text', 'Real-time measurements')
   })
 
-  it('url should end with /', (): void => {
-    cy.url().should('match', /\/$/)
-  })
-
   it('clicking on the logo goes to the home', (): void => {
     cy.get('[data-testid="navigation-menu.logo"]').click()
-    cy.url().should('match', /\/$/)
   })
 
   it('clicking on Home menu option goes to the home', (): void => {
     cy.get('[data-testid="navigation-menu.home"]').click()
-    cy.url().should('match', /\/$/)
   })
 
   it('clicking on the Spanish icon language is changed to Spanish', (): void => {
