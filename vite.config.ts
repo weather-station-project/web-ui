5973dd7c1a38e502a14905d4ac84ed536f5d8b81
David <david.leon.m@gmail.com>
2025-03-28 13:00:41 +0100

    ci

--- vite.config.ts
+++ vite.config.ts
@@ -1,52 +1,57 @@
-import { defineConfig } from 'vite'
+import { ConfigEnv, defineConfig, loadEnv, UserConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    watch: {
-      usePolling: true,
-    },
-    host: true,
-    strictPort: true,
-    port: 5173,
-    open: false,
-    proxy: {
-      '^/api/.*': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-        secure: false,
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        rewrite: (path: string): string => path.replace(/^\/api/, ''),
-        configure: (proxy): void => {
-          proxy.on('error', (err): void => {
-            console.error('Proxy error: ', err)
-          })
-          proxy.on('proxyReq', (proxyReq, req): void => {
-            if (req.url === '/auth') {
-              const bodyData: string = JSON.stringify({ login: 'dashboard', password: '123456' })
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
 
-              proxyReq.setHeader('content-length', bodyData.length)
-              proxyReq.write(bodyData)
-              proxyReq.end()
-            }
-            console.log(`Sending request with method ${req.method} to ${req.url}`)
-          })
-          proxy.on('proxyRes', (proxyRes, req): void => {
-            console.log(`Received (${proxyRes.statusCode}) ${proxyRes.statusMessage} from the target ${req.url}`)
-          })
+  return {
+    plugins: [react()],
+    server: {
+      watch: {
+        usePolling: true,
+      },
+      host: true,
+      strictPort: true,
+      port: 5173,
+      open: false,
+      proxy: {
+        '^/api/.*': {
+          target: 'http://localhost:8080',
+          changeOrigin: true,
+          secure: false,
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          rewrite: (path: string): string => path.replace(/^\/api/, ''),
+          configure: (proxy): void => {
+            proxy.on('error', (err): void => {
+              console.error('Proxy error: ', err)
+            })
+            proxy.on('proxyReq', (proxyReq, req): void => {
+              if (req.url === '/auth') {
+                const bodyData: string = JSON.stringify({ login: 'dashboard', password: '123456' })
+
+                proxyReq.setHeader('content-length', bodyData.length)
+                proxyReq.write(bodyData)
+                proxyReq.end()
+              }
+              console.log(`Sending request with method ${req.method} to ${req.url}`)
+            })
+            proxy.on('proxyRes', (proxyRes, req): void => {
+              console.log(`Received (${proxyRes.statusCode}) ${proxyRes.statusMessage} from the target ${req.url}`)
+            })
+          },
+        },
+        '/socket.io': {
+          target: 'http://localhost:8081',
+          changeOrigin: true,
+          secure: false,
+          ws: true,
         },
       },
-      '/socket.io': {
-        target: 'http://localhost:8081',
-        changeOrigin: true,
-        secure: false,
-        ws: true,
-      },
     },
-  },
-  build: { rollupOptions: { external: ['/env.js'] } },
+    logLevel: 'silent',
+    build: { rollupOptions: { external: ['/env.js'] } },
+  }
 })
